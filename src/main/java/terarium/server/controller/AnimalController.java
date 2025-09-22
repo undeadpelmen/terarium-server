73f1f151784b3e68e312adaa59527e93beca6dac
undeadpelmen <undeadpelmen@gmail.com>
2025-09-22 15:03:00 +0300

    add animal api reference

--- src/main/java/terarium/server/controller/AnimalController.java
+++ src/main/java/terarium/server/controller/AnimalController.java
@@ -1,13 +1,50 @@
 package terarium.server.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
+import terarium.server.dto.Animal.CreateAnimalDto;
+import terarium.server.dto.Animal.GetAnimalDto;
+import terarium.server.dto.Animal.UpdateAnimalDto;
 import terarium.server.service.AnimalService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 public class AnimalController {
     
     @Autowired
     private AnimalService animalService;
+    
+    @GetMapping("/animal")
+    public List<GetAnimalDto> GetAllAnimals() {
+        return animalService.GetAllAnimals();
+    }
+    
+    @GetMapping("/animal/{animalId}")
+    public GetAnimalDto getAnimalById(@PathVariable int animalId) {
+        return animalService.GetAnimalById(animalId);
+    }
+    
+    @PostMapping("/animal")
+    public GetAnimalDto createAnimal(@RequestBody CreateAnimalDto createAnimalDto) {
+        return animalService.CreateAnimal(createAnimalDto);
+    }
+    
+    @DeleteMapping("/animal/{animalId}")
+    public GetAnimalDto deleAnimal(@PathVariable("animalId") int animalId){
+        return animalService.DeleteAnimal(animalId);
+    }
+    
+    @PutMapping("animal/{animalId}")
+    public GetAnimalDto updateAnimal(@PathVariable int animalId, @RequestBody UpdateAnimalDto entity) {
+        return animalService.UpdateAnimal(entity, animalId);
+    }
 }
--- src/main/java/terarium/server/service/AnimalService.java
+++ src/main/java/terarium/server/service/AnimalService.java
@@ -1,7 +1,32 @@
 package terarium.server.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import terarium.server.dto.Animal.CreateAnimalDto;
+import terarium.server.dto.Animal.GetAnimalDto;
+import terarium.server.dto.Animal.UpdateAnimalDto;
 
 @Service
 public class AnimalService {
+    public List<GetAnimalDto> GetAllAnimals(){
+        return null;
+    }
+    
+    public GetAnimalDto GetAnimalById(int id){
+        return null;
+    }
+    
+    public GetAnimalDto CreateAnimal(CreateAnimalDto createAnimalDto){
+        return null;
+    }
+    
+    public GetAnimalDto DeleteAnimal(int id){
+        return null;
+    }
+    
+    public GetAnimalDto UpdateAnimal(UpdateAnimalDto animal, int id){
+        return null;
+    }
 }
